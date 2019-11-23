de5bae26033b9aca212d49a9019e9c8d5a216f5d
dzucconi <mail@damonzucconi.com>
2019-11-23 14:10:51 -0500

    Refines script

---
+++ catalog.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import glob
+from progress.bar import ChargingBar
+
+Image.MAX_IMAGE_PIXELS = None
+
+filenames = glob.glob('./selects/*.png')
+
+size = 2000, 2000
+
+progress = ChargingBar('Processing', max=len(filenames))
+
+def format(path):
+  progress.next()
+  img = Image.open(path)
+  width, height = img.size
+
+  print_width = f'{round(width / 300, 2)}in'
+  print_height = f'{round(height / 300, 2)}in'
+
+  filename = path.split('/')[-1]
+  new_filename = f'./catalog/{print_width}x{print_height}__{filename}'
+  img.thumbnail(size, Image.ANTIALIAS)
+  img.save(new_filename, 'PNG')
+
+images = list(map(format, filenames))
+
+progress.finish()
--- script.py
+++ script.py
@@ -1,23 +1,87 @@
+#!/usr/bin/env python3
+
 import sys
 import random
+import time
+import size
+import hue
+import numpy as np
 from PIL import Image
+from progress.bar import ChargingBar
+from glob import glob
 
-BLOCKLEN = int(sys.argv[2])
+CONFIG = {
+    'width': 9000,  # 30
+    'height': 11000,  # 36.667
+    'size': 15,
+    'anchor': 'center',
+    'filename': f'output_{int(time.time())}',
+    'images':  list(map(Image.open, glob('source/x/*.jpg'))) +
+    [
+        Image.open('source/x.jpg'),
+    ],
+}
 
-img = Image.open('./source/' + sys.argv[1])
-width, height = img.size
+np.random.shuffle(CONFIG.get('images'))
 
-xblock = width / BLOCKLEN
-yblock = height / BLOCKLEN
-blockmap = [(xb * BLOCKLEN, yb * BLOCKLEN, (xb+1) * BLOCKLEN, (yb+1) * BLOCKLEN)
-  for xb in xrange(xblock) for yb in xrange(yblock)]
+cropping_bar = ChargingBar('Cropping', max=len(CONFIG.get('images')))
 
-shuffle = list(blockmap)
-shuffle.reverse()
 
-result = Image.new(img.mode, (width, height))
-for box, sbox in zip(blockmap, shuffle):
-  c = img.crop(sbox)
-  result.paste(c, box)
+def cropit(img):
+    cropping_bar.next()
+    return size.crop(img, (CONFIG.get('width'), CONFIG.get('height')), anchor=CONFIG.get('anchor'))
 
-result.save('./processed/' + sys.argv[1])
+
+CROPPED = list(map(cropit, CONFIG.get('images')))
+
+cropping_bar.finish()
+
+x = CONFIG.get('width') // CONFIG.get('size')
+y = CONFIG.get('height') // CONFIG.get('size')
+
+print('Building map...')
+
+mapping = [(
+    xb * CONFIG.get('size'),
+    yb * CONFIG.get('size'),
+    (xb + 1) * CONFIG.get('size'),
+    (yb + 1) * CONFIG.get('size'),
+) for yb in range(y) for xb in range(x)]
+
+coords = list(mapping)
+
+print('Building output...')
+
+output = Image.new('RGB', (CONFIG.get('width'), CONFIG.get('height')))
+
+SET = CROPPED
+
+rows = 0
+offset = 0
+processing_bar = ChargingBar('Processing', max=len(coords))
+
+for i, box in enumerate(coords, start=1):
+    processing_bar.next()
+
+    # new_row = box[0] == 0
+    # if new_row:
+    #     # offset = (rows % (len(SET) // 2))
+    #     offset = rows % len(SET)
+    #     rows = rows + 1
+
+    which = (i + offset) % len(SET)
+
+    # degree =  360 / ((i % (len(SET) - 1.0)) + 1.0)
+    # degree =  360 / (i % (len(SET)) + 1.0)
+    # degree = (90, 180, 270, 0)[i % 4]
+    # crop = SET[which].crop(box)
+    # colored = hue.colorize(crop, degree)
+
+    crop = SET[which].crop(box)
+    output.paste(crop, box)
+
+processing_bar.finish()
+
+print('Saving...')
+output.save(f'processed/{CONFIG.get("filename")}.png', 'PNG', quality=100)
+print('Done.')
